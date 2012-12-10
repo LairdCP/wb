9812a80e2f0263b36f06217759a098b6e7f4f6dc
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2012-12-10 23:38:12 +0000

    Simplify wbfs.mk and detect erros at this level, at least. The scripts can still fail unnoticed though.
    git-svn-id: svn://10.1.10.7/dev_linux/wb45n/branches/tarballs_and_diffs/dev@15930 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc/wbfs/wbfs.mk
+++ buildroot/package/sdc/wbfs/wbfs.mk
@@ -14,47 +14,11 @@ WBFS_DEPENDENCIES = zlib
 
 WBFS_DIR:=package/sdc/wbfs
 
-ifeq ($(WB_PLATFORM),)
-	# issue warning
-endif
-
 define WBFS_INSTALL_TARGET_CMDS
-	@( \
-	  echo "--- Filesystem tuning for $${WB_PLATFORM} . . ." ;\
-	  wb=$${WB_PLATFORM%%_*} ;\
-	  if [ "$${wb}" != "$${WB_PLATFORM}" ] \
-	  && [ "$${wb}_" != "$${WB_PLATFORM}" ] ;\
-	  then \
-	    wb=$${WB_PLATFORM} ;\
-	  else \
-	    wb=$${wb}_basic ;\
-	  fi ;\
-	  echo -e "\twb-platform_name: $${wb}" ;\
-     cd $(WBFS_DIR) && echo -e "\twbfs: `pwd`" ;\
-	  \
-	  if [ -d "wb_common" ] ;\
-	  then \
-	    echo -e "\n--* Copying wbfs/wb_common/* \n\t-> $(TARGET_DIR)" ;\
-	    rsync -rlp --exclude="\.[se][vm][np]*" wb_common/* $(TARGET_DIR) ;\
-	  fi ;\
-	  if [ -x "wb_common.sh" ] ;\
-	  then \
-	    echo "--> Running wb_common.sh" ;\
-	    ./wb_common.sh $(TARGET_DIR) || echo ERROR ;\
-	  fi ;\
-	  \
-	  if [ -d "$${wb}" ] ;\
-	  then \
-	    echo -e "\n--* Copying wbfs/$${wb}/* \n\t-> $(TARGET_DIR)" ;\
-	    rsync -rlp --exclude="\.[se][vm][np]*" $${wb}/* $(TARGET_DIR) ;\
-	  fi ;\
-	  if [ -x "$${wb}.sh" ] ;\
-	  then \
-	    echo "--> Running $${wb}.sh" ;\
-	    sh $${wb}.sh $(TARGET_DIR) || echo ERROR ;\
-	  fi ;\
-	  echo ;\
-	)
+	cd $(WBFS_DIR) && rsync -rlp --exclude="\.[se][vm][np]*" wb_common/* $(TARGET_DIR)
+	cd $(WBFS_DIR) && ./wb_common.sh $(TARGET_DIR)
+	cd $(WBFS_DIR) && rsync -rlp --exclude="\.[se][vm][np]*" $(BR2_SDC_PLATFORM)_basic/* $(TARGET_DIR)
+    cd $(WBFS_DIR) && ./$(BR2_SDC_PLATFORM)_basic.sh $(TARGET_DIR)
 endef
 
 define WBFS_UNINSTALL_TARGET_CMDS
