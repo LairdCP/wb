6a613c7c2f51263514b27e5ae7d1c57368cb8b83
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2013-01-10 17:13:20 +0000

    Introduce post_build_common.sh.
    git-svn-id: svn://10.1.10.7/dev_linux/wb45n/branches/tarballs_and_diffs/dev@17929 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

---
+++ buildroot/board/sdc/post_build_common.sh
@@ -0,0 +1,28 @@
+TARGETDIR=$1
+
+echo "COMMON POST BUILD script: starting..."
+
+# enable tracing and exit on errors
+set -x -e
+
+# Set root password to ’root’. Password generated with
+# mkpasswd, from the ’whois’ package in Debian/Ubuntu.
+## sed -i ’s%^root::%root:8kfIfYHmcyQEE:%’ $TARGETDIR/etc/shadow
+
+# Application/log file mount point
+#mkdir -p $TARGETDIR/applog
+## grep -q "^/dev/mtdblock7" $TARGETDIR/etc/fstab || \
+## echo "/dev/mtdblock7\t\t/applog\tjffs2\tdefaults\t\t0\t0" \
+## >> $TARGETDIR/etc/fstab
+
+# Copy the rootfs additions
+tar c --exclude=.svn -C board/sdc/wb40n/rootfs-additions/ . | tar x -C $TARGETDIR/
+
+# copy my RSA public key to device
+install -d -m 700 $TARGETDIR/root/.ssh
+install -m 600 $HOME/.ssh/id_rsa.pub $TARGETDIR/root/.ssh/authorized_keys
+
+# delete the default ssh init file
+rm -f $TARGETDIR/etc/init.d/S50sshd
+
+echo "COMMON POST BUILD script: done."
--- buildroot/board/sdc/wb45n/post_build.sh
+++ buildroot/board/sdc/wb45n/post_build.sh
@@ -1,14 +1,9 @@
 TARGETDIR=$1
 
-# Set root password to ’root’. Password generated with
-# mkpasswd, from the ’whois’ package in Debian/Ubuntu.
-## sed -i ’s%^root::%root:8kfIfYHmcyQEE:%’ $TARGETDIR/etc/shadow
+echo "WB45n POST BUILD script: starting..."
 
-# Application/log file mount point
-#mkdir -p $TARGETDIR/applog
-## grep -q "^/dev/mtdblock7" $TARGETDIR/etc/fstab || \
-## echo "/dev/mtdblock7\t\t/applog\tjffs2\tdefaults\t\t0\t0" \
-## >> $TARGETDIR/etc/fstab
+# source the common post build script
+parentdir="`dirname "$0"`/.."
+source "$parentdir/post_build_common.sh" "$TARGETDIR"
 
-# Copy the rootfs additions
-tar c --exclude=.svn -C board/sdc/wb45n/rootfs-additions/ . | tar x -C $TARGETDIR/
+echo "WB45n POST BUILD script: done."
