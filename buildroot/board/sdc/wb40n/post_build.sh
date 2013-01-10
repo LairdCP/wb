e809e781a4a510091a7ab1a0ed06d0d3207e39ca
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2013-01-10 20:26:17 +0000

    Simpify the invocation post_build_common.sh.
    git-svn-id: svn://10.1.10.7/dev_linux/wb45n/branches/tarballs_and_diffs/dev@17940 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/board/sdc/wb40n/post_build.sh
+++ buildroot/board/sdc/wb40n/post_build.sh
@@ -3,7 +3,6 @@ TARGETDIR=$1
 echo "WB40n POST BUILD script: starting..."
 
 # source the common post build script
-parentdir="`dirname "$0"`/.."
-source "$parentdir/post_build_common.sh" "$TARGETDIR"
+source "board/sdc/post_build_common.sh" "$TARGETDIR"
 
 echo "WB40n POST BUILD script: done."
