4b5f553b79451310d6e4f15ebc4771186262acec
Eric Bentley <Eric.Bentley@lairdtech.com>
2013-01-25 15:15:22 +0000

    add new firmware files
    git-svn-id: svn://10.1.10.7/dev_linux/wb/dev@18171 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc-closed-source/dhd/dhd.mk
+++ buildroot/package/sdc-closed-source/dhd/dhd.mk
@@ -17,7 +17,8 @@ DHD_MAKE_ENV = CC="$(TARGET_CC)" \
 
 DHD_BT_FW1 = BCM4329B1_002.002.023.0924.1027.hcd
 DHD_BT_FW2 = BCM4329B1_002.002.023.0924.1032.hcd
-DHD_FIRMWARE_FILENAME = 4329b1-4-220-55-sdio-ag-cdc-roml-reclaim-11n-wme-minccx-extsup-aoe-pktfilter-keepalive.bin
+DHD_FIRMWARE_FILENAME_1 = 4329b1_4_220_55_sdio-ag-cdc-full11n-reclaim-roml-wme-aoe-pktfilter-keepalive-extsup.bin
+DHD_FIRMWARE_FILENAME_2 = 41329b1_4_220_55_sdio-ag-cdc-reclaim-roml-wme-aoe-pktfilter-keepalive-ccxv4-extsup-minccx.bin
 DHD_DEBUG = -debug
 DHD_TARGET_DIR = $(O)/sdcbins
 
@@ -36,9 +37,12 @@ define DHD_INSTALL_TARGET_CMDS
 	$(MAKE) --no-print-directory -C $(LINUX_DIR) kernelversion ARCH=arm CROSS_COMPILE="$(TARGET_CROSS)" > $(@D)/kernel.version
 	$(INSTALL) -D -m 644 $(@D)/open-src/src/dhd/linux/dhd-cdc-sdmmc-cfg80211-gpl$(DHD_DEBUG)-`cat $(@D)/kernel.release`/dhd.ko  \
         $(DHD_TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra/drivers/net/wireless/dhd.ko
-    $(INSTALL) -D -m 644 $(@D)/firmware/4329b1/$(DHD_FIRMWARE_FILENAME) \
-        $(DHD_TARGET_DIR)/etc/summit/firmware/$(DHD_FIRMWARE_FILENAME)
-    ln -sf $(DHD_FIRMWARE_FILENAME) $(DHD_TARGET_DIR)/etc/summit/firmware/fw
+   $(INSTALL) -D -m 644 $(@D)/firmware/4329b1/$(DHD_FIRMWARE_FILENAME_1) \
+        $(DHD_TARGET_DIR)/etc/summit/firmware/$(DHD_FIRMWARE_FILENAME_1)
+   $(INSTALL) -D -m 644 $(@D)/firmware/4329b1/$(DHD_FIRMWARE_FILENAME_2) \
+        $(DHD_TARGET_DIR)/etc/summit/firmware/$(DHD_FIRMWARE_FILENAME_2)
+    ln -sf $(DHD_FIRMWARE_FILENAME_1) $(DHD_TARGET_DIR)/etc/summit/firmware/fw
+    ln -sf $(DHD_FIRMWARE_FILENAME_2) $(DHD_TARGET_DIR)/etc/summit/firmware/fw-ccx
     $(INSTALL) -D -m 644 $(@D)/nvram/production.nv $(DHD_TARGET_DIR)/etc/summit/nvram/nv
     $(INSTALL) -D -m 755 $(@D)/open-src/src/wl/exe/wl $(DHD_TARGET_DIR)/usr/bin/wl
     $(INSTALL) -D -m 644 $(@D)/firmware/bt/$(DHD_BT_FW1) $(DHD_TARGET_DIR)/etc/summit/firmware/$(DHD_BT_FW1)
