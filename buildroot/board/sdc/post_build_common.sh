9a3acecc1bf9f51edd0d02d998d7d909cb440de2
Jon Hefling <Jon.Hefling@lairdtech.com>
2013-02-21 16:57:46 +0000

    added some permissions handling for important files and minor cleanup
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@18469 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/board/sdc/post_build_common.sh
+++ buildroot/board/sdc/post_build_common.sh
@@ -15,10 +15,11 @@ set -x -e
 ## echo "/dev/mtdblock7\t\t/applog\tjffs2\tdefaults\t\t0\t0" \
 ## >> $TARGETDIR/etc/fstab
 
-# delete the default ssh init file, real one is in init.d/opt
+# delete the default ssh init file
+# real version is in init.d/opt and works w/ inetd
 rm -f $TARGETDIR/etc/init.d/S50sshd
 
-# need to fix this init-script before ready to use
+# TODO: init-script is not ready for use
 rm -f $TARGETDIR/etc/init.d/openvpn
 
 # remove bash cruft
@@ -36,22 +37,34 @@ rm -f $TARGETDIR/etc/init.d/rcK
 tar c --exclude=.svn -C board/sdc/rootfs-additions-common/ . | tar x -C $TARGETDIR/
 
 # install libnl*.so.3 links
-(   cd "$TARGETDIR/usr/lib" &&
-    ln -sf libnl-3.so libnl.so.3 &&
-    ln -sf libnl-genl-3.so libnl-genl.so.3  )
+( cd "$TARGETDIR/usr/lib" \
+  && ln -sf libnl-3.so libnl.so.3 \
+  && ln -sf libnl-genl-3.so libnl-genl.so.3 )
 
 # Services to disable by default
 chmod a-x "$TARGETDIR/etc/init.d/S59snmpd"
 chmod a-x "$TARGETDIR/etc/init.d/S99lighttpd"
 
 # copy the freshly built sdc binaries
-if [ -d output/$BR2_SDC_PLATFORM/sdcbins ]; then
-    cp -a output/$BR2_SDC_PLATFORM/sdcbins/* $TARGETDIR/
+if [ -d output/$BR2_SDC_PLATFORM/sdcbins ]
+then
+  cp -a output/$BR2_SDC_PLATFORM/sdcbins/* $TARGETDIR/
 fi
 
 # create missing symbolic link
-# TODO: we shouldn't have to do this here, this is a temporary workaround
-(cd $TARGETDIR/usr/lib && ln -sf "libsdc_sdk.so.1.0" "libsdc_sdk.so.1")
+# TODO: shouldn't have to do this here, temporary workaround
+( cd $TARGETDIR/usr/lib \
+  && ln -sf libsdc_sdk.so.1.0 libsdc_sdk.so.1 )
+
+# set common permissions
+# handled here, instead of in a device_table.txt, which is processed later
+chmod 4755 /bin/busybox
+chmod 600 /etc/shadow
+chmod 644 /etc/passwd
+chmod -R 755 /etc
+chmod -R 1777 /tmp
+chmod -R 2744 /home/summit
+chown -R summit:summit /home/summit
 
 # create firmware release file
 echo "SDC Linux Release `date +%Y%m%d`" \
