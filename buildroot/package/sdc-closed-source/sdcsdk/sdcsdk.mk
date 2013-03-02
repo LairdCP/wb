3e411748c29118ec83000817560a811be3231e8d
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2013-03-02 13:27:47 +0000

    Add host-pkg-config to the list of dependencies of sdcsdk.
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@18633 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc-closed-source/sdcsdk/sdcsdk.mk
+++ buildroot/package/sdc-closed-source/sdcsdk/sdcsdk.mk
@@ -7,7 +7,7 @@
 SDCSDK_VERSION = local
 SDCSDK_SITE = package/sdc-closed-source/externals/sdk
 SDCSDK_SITE_METHOD = local
-SDCSDK_DEPENDENCIES = libnl
+SDCSDK_DEPENDENCIES = libnl host-pkg-config
 SDCSDK_INSTALL_STAGING = YES
 SDCSDK_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config"
 SDCSDK_TARGET_DIR = $(O)/sdcbins
