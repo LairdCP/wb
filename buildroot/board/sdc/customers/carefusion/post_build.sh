eea00a15218804af67882adde8237514715533a6
Jon Hefling <Jon.Hefling@lairdtech.com>
2013-04-11 11:48:27 +0000

    remove unnecessary www and bcm content add more comments
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@19026 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/board/sdc/customers/carefusion/post_build.sh
+++ buildroot/board/sdc/customers/carefusion/post_build.sh
@@ -1,16 +1,29 @@
 set -e -x
 
 TARGETDIR=$1
-#
-# applied in order:
-# rootfs-additions-common
-# wb40n/rootfs-additions
-# carefusion/rootfs-additions
-#
+echo
 echo "Carefusion POST BUILD ..."
 
+# This post-build script will invoke wb40n post-build, which will also invoke
+# the common post-build...
+#
+# Content will be copied from each rootfs, in order:
+#  1. rootfs-additions-common
+#  2. wb40n/rootfs-additions
+#  3. carefusion/rootfs-additions
+#
 # source the respective platform post build script
 source "board/sdc/wb40n/post_build.sh" "$TARGETDIR"
+
+# remove lighttpd and other cruft
+rm -fr $TARGETDIR/var/www
+rm -fr $TARGETDIR/etc/lighttpd
+
+# remove bluetooth cruft
+rm -f $TARGETDIR/etc/init.d/opt/S95bluetooth
+rm -f $TARGETDIR/etc/summit/bluetooth.conf
+rm -f $TARGETDIR/etc/summit/BCM*.hcd
+
 
 # copy the product specific rootfs-additions
 tar c --exclude=.svn -C board/sdc/customers/carefusion/rootfs-additions/ . \
