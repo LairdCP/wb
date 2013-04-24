745af2d3a8c013d67ace9b5ee77ef17297e13ced
Jon Hefling <Jon.Hefling@lairdtech.com>
2013-04-24 16:56:29 +0000

    makefile for sdc2u_fips
    git-svn-id: svn://10.1.10.7/dev_linux/wb/branches/fips@19168 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc-closed-source/sdc2u_fips/sdc2u.mk
+++ buildroot/package/sdc-closed-source/sdc2u_fips/sdc2u.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 SDC2U_VERSION = local
-SDC2U_SITE = package/sdc-closed-source/externals/sdc2u
+SDC2U_SITE = package/sdc-closed-source/externals/sdc2u_fips
 SDC2U_SITE_METHOD = local
 SDC2U_DEPENDENCIES = linux
 SDC2U_MAKE_ENV = CC="$(TARGET_CC)" \
@@ -23,9 +23,7 @@ endef
 
 define SDC2U_BUILD_CMDS
 	#$(MAKE) -C $(@D) clean
-    #make ARCH=arm CROSS_COMPILE=arm-sdc-linux-gnueabi- LINUXDIR=~/dev_linux/wb40n/basic/kernel/ dhd-cdc-sdmmc-cfg80211-gpl
-	$(SDC2U_MAKE_ENV) $(MAKE) -C $(@D)/open-src/src/dhd/linux V=1 dhd-cdc-sdmmc-cfg80211-gpl$(SDC2U_DEBUG)
-    $(MAKE) -C $(@D)/open-src/src/wl/exe CC="$(TARGET_CC)"
+	$(SDC2U_MAKE_ENV) $(MAKE) -C $(@D) sdcu ccm_modules
 endef
 
 define SDC2U_INSTALL_TARGET_CMDS
@@ -33,9 +31,9 @@ define SDC2U_INSTALL_TARGET_CMDS
 	$(MAKE) --no-print-directory -C $(LINUX_DIR) kernelversion ARCH=arm CROSS_COMPILE="$(TARGET_CROSS)" > $(@D)/kernel.version
 	
 	$(INSTALL) -D -m 644 $(@D)/sdc2u.ko \
-        $(SDC2U_TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra/drivers/net/wireless/dhd.ko
+        $(SDC2U_TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra/sdc2u.ko
 
-    $(INSTALL) -D -m 755 $(@D)/src/wl/exe/wl $(SDC2U_TARGET_DIR)/usr/bin/wl
+    $(INSTALL) -D -m 755 $(@D)/sdcu $(SDC2U_TARGET_DIR)/usr/bin/sdcu
 endef
 
 #define SDC2U_UNINSTALL_TARGET_CMDS
