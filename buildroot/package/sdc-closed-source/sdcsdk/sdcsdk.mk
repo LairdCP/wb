5b17e1033d1b250eb176c04a7a432a44c4eff8e6
Dan Kephart <Dan.Kephart@lairdtech.com>
2013-06-14 16:16:16 +0000

    Adding lrd_platspec to staging area and target
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@19689 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc-closed-source/sdcsdk/sdcsdk.mk
+++ buildroot/package/sdc-closed-source/sdcsdk/sdcsdk.mk
@@ -28,22 +28,29 @@ define SDCSDK_BUILD_CMDS
 endef
 
 define SDCSDK_INSTALL_STAGING_CMDS
+	rm -r $(STAGING_DIR)/usr/lib/liblrd_platspec.so*
     rm -f $(STAGING_DIR)/usr/lib/libsdc_sdk.so*
 	$(INSTALL) -D -m 0755 $(@D)/libsdc_sdk.so.1.0 $(STAGING_DIR)/usr/lib/
-    cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1.0 libsdc_sdk.so.1
+	$(INSTALL) -D -m 0755 $(@D)/liblrd_platspec.so.1.0 $(STAGING_DIR)/usr/lib/
+    cd  $(STAGING_DIR)/usr/lib/ && ln -s liblrd_platspec.so.1.0 liblrd_platspec.so.1
+    cd  $(STAGING_DIR)/usr/lib/ && ln -s liblrd_platspec.so.1 liblrd_platspec.so
+	cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1.0 libsdc_sdk.so.1
     cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1 libsdc_sdk.so
 	$(INSTALL) -D -m 0755 $(@D)/include/sdc_sdk.h \
                           $(@D)/include/sdc_events.h \
+						  $(@D)/include/lrd_platspec.h \
                           $(@D)/include/config_strings.h \
                           $(STAGING_DIR)/usr/include/
 endef
 
 define SDCSDK_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libsdc_sdk.so.1.0 $(SDCSDK_TARGET_DIR)/usr/lib/libsdc_sdk.so.1.0
+	$(INSTALL) -D -m 0755 $(@D)/liblrd_platspec.so.1.0 $(SDCSDK_TARGET_DIR)/usr/lib/liblrd_platspec.so.1.0
 endef
 
 define SDCSDK_UNINSTALL_TARGET_CMDS
 	rm -f $(SDCSDK_TARGET_DIR)/usr/lib/libsdc_sdk.so.1.0
+	rm -f $(SDCSDK_TARGET_DIR)/usr/lib/liblrd_platspec.so.1.0
 endef
 
 $(eval $(generic-package))
