27e81084bec4630d3d7a1beae2b115388621ce7a
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2013-06-17 09:48:33 +0000

    Update msd45n-binaries package to install the new lrd_platspec library and header files. Also made it install the closed source libraries and header files to the staging area.
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@19702 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/package/sdc/msd45n-binaries/msd45n-binaries.mk
+++ buildroot/package/sdc/msd45n-binaries/msd45n-binaries.mk
@@ -1,4 +1,4 @@
-MSD45N_BINARIES_SITE = http://boris.corp.lairdtech.com/builds/Linux/msd45n/beta
+MSD45N_BINARIES_SITE = http://boris.corp.lairdtech.com/scratch/releases/msd45n_fips/laird/beta.0.1
 MSD45N_BINARIES_VERSION = $(call qstrip,$(BR2_MSD45N_BINARIES_VERSION))
 MSD45N_BINARIES_COMPANY_PROJECT = $(call qstrip,$(BR2_MSD45N_BINARIES_COMPANY_PROJECT))
 MSD45N_BINARIES_SOURCE = msd45n-$(MSD45N_BINARIES_COMPANY_PROJECT)-$(MSD45N_BINARIES_VERSION).tar.bz2
@@ -18,6 +18,22 @@ define MSD45N_BINARIES_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 755 $(@D)/usr/bin/sdcsupp $(TARGET_DIR)/usr/bin/sdcsupp
     mkdir -p $(TARGET_DIR)/usr/lib
     $(INSTALL) -m 755 $(@D)/usr/lib/libsdc_sdk.so* $(TARGET_DIR)/usr/lib/
+    $(INSTALL) -m 755 $(@D)/usr/lib/liblrd_platspec.so* $(TARGET_DIR)/usr/lib/
+endef
+
+define MSD45N_BINARIES_INSTALL_STAGING_CMDS
+	rm -f $(STAGING_DIR)/usr/lib/liblrd_platspec.so*
+    rm -f $(STAGING_DIR)/usr/lib/libsdc_sdk.so*
+	$(INSTALL) -D -m 0755 $(@D)/usr/lib/libsdc_sdk.so.1.0 $(STAGING_DIR)/usr/lib/
+	$(INSTALL) -D -m 0755 $(@D)/usr/lib/liblrd_platspec.so.1.0 $(STAGING_DIR)/usr/lib/
+    cd  $(STAGING_DIR)/usr/lib/ && ln -s liblrd_platspec.so.1.0 liblrd_platspec.so.1
+    cd  $(STAGING_DIR)/usr/lib/ && ln -s liblrd_platspec.so.1 liblrd_platspec.so
+	cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1.0 libsdc_sdk.so.1
+    cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1 libsdc_sdk.so
+	$(INSTALL) -D -m 0755 $(@D)/include/sdc_sdk.h \
+                          $(@D)/include/sdc_events.h \
+						  $(@D)/include/lrd_platspec.h \
+                          $(STAGING_DIR)/usr/include/
 endef
 
 define MSD45N_BINARIES_INSTALL_FIPS_BINARIES
