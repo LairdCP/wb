4bad063ab8c569a3dc7f5ed7259bcb0d4243f9b0
Dimitrios Siganos <Dimitrios.Siganos@lairdtech.com>
2013-06-17 10:20:58 +0000

    Fix post_image script for msd45n.
    git-svn-id: svn://10.1.10.7/dev_linux/wb/trunk@19706 ce631cd2-4ded-b346-a5c6-2b5c1f60751c

--- buildroot/board/sdc/msd45n/post_image.sh
+++ buildroot/board/sdc/msd45n/post_image.sh
@@ -7,7 +7,7 @@ echo "MSD45n POST IMAGE script: starting..."
 # enable tracing and exit on errors
 set -x -e
 
-test -z "$BR2_SDC_PLATFORM" && export BR2_SDC_PRODUCT=msd45n
+test -z "$BR2_SDC_PRODUCT" && export BR2_SDC_PRODUCT=msd45n
 TARFILE="$IMAGESDIR/$BR2_SDC_PRODUCT.tar"
 
 tar cf "$TARFILE" -C "$IMAGESDIR" rootfs.tar
@@ -16,4 +16,4 @@ tar f "$TARFILE" -C "$STAGING_DIR/usr" -u include/sdc_events.h
 tar f "$TARFILE" -C "$STAGING_DIR/usr" -u include/lrd_platspec.h
 bzip2 -f "$TARFILE"
 
-echo "MSD45n POST BUILD script: done."
+echo "MSD45n POST IMAGE script: done."
