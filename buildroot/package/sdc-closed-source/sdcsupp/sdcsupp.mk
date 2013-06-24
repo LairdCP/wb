d09ce04d9217ead5b82a32e039b18d227de1ce2c
Dimitrios Siganos <dimitris@siganos.org>
2013-06-24 18:37:32 +0100

    Handle extra wpa_supplicant directory.
    The move to git introduced an extra wpa_supplicant directory
    that wasn't there in svn. This commits fixes sdcsupp.mk to
    reflect that change.

--- buildroot/package/sdc-closed-source/sdcsupp/sdcsupp.mk
+++ buildroot/package/sdc-closed-source/sdcsupp/sdcsupp.mk
@@ -30,23 +30,23 @@ else
 endif
 
 define SDCSUPP_BUILD_CMDS
-    cp $(@D)/wpa_supplicant/config_openssl $(@D)/wpa_supplicant/.config
-    $(MAKE) -C $(@D)/wpa_supplicant clean
+    cp $(@D)/wpa_supplicant/wpa_supplicant/config_openssl $(@D)/wpa_supplicant/wpa_supplicant/.config
+    $(MAKE) -C $(@D)/wpa_supplicant/wpa_supplicant clean
     CFLAGS="-I$(STAGING_DIR)/usr/include/libnl3 $(TARGET_CFLAGS) -MMD -Wall -g" \
-        $(MAKE) -C $(@D)/wpa_supplicant V=1 NEED_TLS_LIBDL=1 $(SDCSUPP_FIPS) \
+        $(MAKE) -C $(@D)/wpa_supplicant/wpa_supplicant V=1 NEED_TLS_LIBDL=1 $(SDCSUPP_FIPS) \
             $(SDCSUPP_RADIO_FLAGS) CROSS_COMPILE="$(TARGET_CROSS)"
-    $(TARGET_CROSS)objcopy -S $(@D)/wpa_supplicant/wpa_supplicant $(@D)/wpa_supplicant/sdcsupp
-    #(cd $(@D)/wpa_supplicant && CROSS_COMPILE=arm-sdc-linux-gnueabi ./sdc-build-linux.sh 4 1 2 3 1)
+    $(TARGET_CROSS)objcopy -S $(@D)/wpa_supplicant/wpa_supplicant/wpa_supplicant $(@D)/wpa_supplicant/wpa_supplicant/sdcsupp
+    #(cd $(@D)/wpa_supplicant/wpa_supplicant && CROSS_COMPILE=arm-sdc-linux-gnueabi ./sdc-build-linux.sh 4 1 2 3 1)
 endef
 
 ifeq ($(BR2_PACKAGE_SDCSUPP_WPA_CLI),y)
 define SDCSUPP_INSTALL_WPA_CLI
-	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/wpa_cli $(SDCSUPP_TARGET_DIR)/usr/bin/wpa_cli
+	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/wpa_supplicant/wpa_cli $(SDCSUPP_TARGET_DIR)/usr/bin/wpa_cli
 endef
 endif
 
 define SDCSUPP_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/sdcsupp $(SDCSUPP_TARGET_DIR)/usr/bin/sdcsupp
+	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/wpa_supplicant/sdcsupp $(SDCSUPP_TARGET_DIR)/usr/bin/sdcsupp
 	$(SDCSUPP_INSTALL_WPA_CLI)
 endef
 
