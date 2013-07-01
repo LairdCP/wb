fda519d294b606bb2c61b29a3010b0a72b5f7e58
Dimitrios Siganos <dimitris@siganos.org>
2013-07-02 00:36:18 +0100

    ar6kl-tools build bugfix

--- buildroot/package/sdc-closed-source/ar6kl-tools/ar6kl-tools.mk
+++ buildroot/package/sdc-closed-source/ar6kl-tools/ar6kl-tools.mk
@@ -6,7 +6,7 @@
 
 AR6KL_TOOLS_VERSION = local
 AR6KL_TOOLS_SITE    = package/sdc-closed-source/externals/ath6kl_devkit
-AR6KL_TOOLS_METHOD  = local
+AR6KL_TOOLS_SITE_METHOD = local
 
 define AR6KL_TOOLS_BUILD_CMDS
 	$(MAKE) -C $(@D)/Proprietary_tools/libtcmd \
