40d966a5179c4d03e17dd0c6b302384a3e6b0898
Dimitrios Siganos <dimitris@siganos.org>
2013-07-03 01:52:21 +0100

    Create the fw_update compatible files inside the buildroot images folder.

--- images/common.mk
+++ images/common.mk
@@ -6,25 +6,16 @@ URL = http://$(shell hostname)/wb/$(PRODUCT)
 TOPDIR = ../../..
 IMAGES = $(TOPDIR)/buildroot/output/$(PRODUCT)/images
 
-all: fw.txt fw_update fw_select
+fw.txt: copyall
+	$(TOPDIR)/buildroot/board/sdc/mkfwtxt.sh $(URL)/$(DATE)
 
-fw_%: $(TOPDIR)/buildroot/board/sdc/rootfs-additions-common/usr/sbin/fw_%
-	cp $+ $@
+copyall:
+	cp $(IMAGES)/kernel.bin .
+	cp $(IMAGES)/rootfs.bin .
+	cp $(IMAGES)/u-boot.bin .
+	cp $(IMAGES)/bootstrap.bin .
+	cp $(IMAGES)/fw_update .
+	cp $(IMAGES)/fw_select .
 
-fw.txt: kernel.bin rootfs.bin bootstrap.bin u-boot.bin
-	$(TOPDIR)/images/mkfwtxt.sh $(URL)/$(DATE)
-
-kernel.bin: $(IMAGES)/uImage
-	cp $+ $@
-
-rootfs.bin: $(IMAGES)/rootfs.ubi
-	cp $+ $@
-
-bootstrap.bin: $(IMAGES)/$(PLATFORM).bin
-	cp $+ $@
-
-u-boot.bin: $(IMAGES)/u-boot.bin
-	cp $+ $@
-
-.PHONY: all
+.PHONY: all copyall
 
