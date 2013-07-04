f8c5ef083039c32ac8515019d9b579436370e0b0
Dimitrios Siganos <dimitris@siganos.org>
2013-07-04 10:26:07 +0100

    sdc_cli makefile and patch cleanup
    Cleaned up the makefile and removed the unused makefile patch.

--- buildroot/package/sdc-closed-source/sdccli/sdccli.mk
+++ buildroot/package/sdc-closed-source/sdccli/sdccli.mk
@@ -19,11 +19,6 @@ SDCCLI_MAKE_ENV = CC="$(TARGET_CC)" \
                   CXX="$(TARGET_CXX)" \
                   ARCH="$(KERNEL_ARCH)" \
                   CFLAGS="$(TARGET_CFLAGS)"
-SDCCLI_TARGET_DIR = $(TARGET_DIR)
-
-#define SDCCLI_CONFIGURE_CMDS
-#    patch -d $(@D) < package/sdc-closed-source/sdccli/makefile.patch
-#endef
 
 define SDCCLI_BUILD_CMDS
     $(MAKE) -C $(@D) clean
@@ -31,11 +26,11 @@ define SDCCLI_BUILD_CMDS
 endef
 
 define SDCCLI_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/bin/sdc_cli $(SDCCLI_TARGET_DIR)/usr/bin/sdc_cli
+	$(INSTALL) -D -m 755 $(@D)/bin/sdc_cli $(TARGET_DIR)/usr/bin/sdc_cli
 endef
 
 define SDCCLI_UNINSTALL_TARGET_CMDS
-	rm -f $(SDCCLI_TARGET_DIR)/usr/bin/sdc_cli
+	rm -f $(TARGET_DIR)/usr/bin/sdc_cli
 endef
 
 $(eval $(generic-package))
