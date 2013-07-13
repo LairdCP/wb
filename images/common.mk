50a4b72fbfd4a1e89de1a3f6f6ad36a57181c1f0
Dimitrios Siganos <dimitris@siganos.org>
2013-07-13 18:58:12 +0100

    Copy a compressed version of rootfs.tar into the images folder.

--- images/common.mk
+++ images/common.mk
@@ -16,6 +16,9 @@ copyall:
 	cp $(IMAGES)/bootstrap.bin .
 	cp $(IMAGES)/fw_update .
 	cp $(IMAGES)/fw_select .
+	cp $(IMAGES)/rootfs.tar .
+	rm rootfs.tar.bz2
+	bzip2 rootfs.tar
 
 .PHONY: all copyall
 
