be69c8925510a4ca3d44c31431c26b40f6b324b9
Dimitrios Siganos <dimitris@siganos.org>
2013-09-19 14:51:27 +0100

    Remove the 2 second delay after wireless start.
    Apparently, we don't need it.

--- buildroot/laird-devel/scripts/run_autosecurity.sh
+++ buildroot/laird-devel/scripts/run_autosecurity.sh
@@ -8,7 +8,6 @@ SCP="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 # run the autosecurity script
 sshpass -psummit $SSH root@$WB45N_ADDRESS wireless stop
-sleep 2
 sshpass -psummit $SCP -r buildroot/laird-devel/scripts/certs/* root@$WB45N_ADDRESS:/etc/ssl
 sshpass -psummit $SCP buildroot/laird-devel/scripts/wfaXX.conf root@$WB45N_ADDRESS:/etc/summit/profiles.conf
 sshpass -psummit $SCP buildroot/laird-devel/scripts/autosecurity.sh root@$WB45N_ADDRESS:/bin/
