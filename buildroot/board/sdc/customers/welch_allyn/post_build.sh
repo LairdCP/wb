34d32d96408b4f37d882672d36a91a45ad8f02d5
Jon Hefling <jon.hefling@lairdtech.com>
2013-10-31 07:56:10 -0500

    Remove customer specific setting from the common /e/n/i file, and apply new customer specific settings.

--- buildroot/board/sdc/customers/welch_allyn/post_build.sh
+++ buildroot/board/sdc/customers/welch_allyn/post_build.sh
@@ -10,8 +10,5 @@ source "board/sdc/wb45n/post_build.sh" "$TARGETDIR"
 # Copy the product specific rootfs additions
 tar c --exclude=.svn -C board/sdc/customers/welch_allyn/rootfs-additions/ . | tar x -C $TARGETDIR/
 
-# Enable Welch Allyn service
-chmod a+x "$TARGETDIR/etc/init.d/S91g_ether"
-chmod a-x "$TARGETDIR/etc/init.d/S27usbhost"
 
 echo "Welch Allyn POST BUILD script: done."
