38602b9301c3077d7bc31e8c5939ee2daf885a47
Dimitrios Siganos <dimitris@siganos.org>
2013-11-12 07:43:06 +0000

    Stop compiling sdc2u linux modules outside of the kernel tree.
    The sdcu package now only builds the sdcu application.
    The kernel objects will be open sourced and added to the linux kernel.
    Bug 4560.

--- buildroot/package/sdc-closed-source/sdc2u/sdc2u.mk
+++ buildroot/package/sdc-closed-source/sdc2u/sdc2u.mk
@@ -14,7 +14,7 @@ SDC2U_SITE = package/sdc-closed-source/externals/sdc2u_fips
 SDC2U_SITE_METHOD = local
 endif
 
-SDC2U_DEPENDENCIES = linux openssl
+SDC2U_DEPENDENCIES = openssl
 
 
 MAKE_ENV = \
@@ -29,21 +29,11 @@ endef
 
 define SDC2U_BUILD_CMDS
 	@echo \ --\> sdcu and sdc2u.ko
-	$(MAKE) $(MAKE_ENV) -C $(@D)/sdc2u
 	$(MAKE) $(MAKE_ENV) -C $(@D)/sdcu
 	@echo \ ---
 endef
 
 define SDC2U_INSTALL_TARGET_CMDS
-	$(MAKE) --no-print-directory $(MAKE_ENV) -C $(LINUX_DIR) kernelrelease > $(@D)/kernel.release
-	$(MAKE) --no-print-directory $(MAKE_ENV) -C $(LINUX_DIR) kernelversion > $(@D)/kernel.version
-	
-	$(INSTALL) -D -m 644 $(@D)/sdc2u/ath6kl_laird.ko \
-        $(TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra/ath6kl_laird.ko
-       
-	$(INSTALL) -D -m 644 $(@D)/sdc2u/sdc2u.ko \
-        $(TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra/sdc2u.ko
-	
 	$(INSTALL) -D -m 755 $(@D)/sdcu/sdcu \
         $(TARGET_DIR)/usr/bin/sdcu
 endef
