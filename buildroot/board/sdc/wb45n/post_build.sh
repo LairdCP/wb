23b1ac77c750c1d0298c4b044a0fb5042d485923
Zachary Coffy <Zachary.Coffy@lairdtech.com>
2013-11-13 13:34:24 -0500

    Add execute permission for php-cgi on the wb45n
    needed for web_lcm
    also fail if files not found

--- buildroot/board/sdc/wb45n/post_build.sh
+++ buildroot/board/sdc/wb45n/post_build.sh
@@ -14,7 +14,8 @@ tar c --exclude=.svn --exclude=.empty -C board/sdc/wb45n/rootfs-additions/ . | t
 [ -f $TARGETDIR/etc/init.d/S??usbhost ] \
 && chmod a-x $TARGETDIR/etc/init.d/S??usbhost
 
-[ -f $TARGETDIR/etc/init.d/S??lighttpd ] \
-&& chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
+chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
+
+chmod a+x $TARGETDIR/usr/bin/php-cgi
 
 echo "WB45n POST BUILD script: done."
