6667c4ba5b440a00d9da4904946d430762e776a0
Zachary Coffy <Zachary.Coffy@lairdtech.com>
2013-11-13 12:14:50 -0500

    Have the web_lcm run on bootup by default on the wb45n

--- buildroot/board/sdc/wb45n/post_build.sh
+++ buildroot/board/sdc/wb45n/post_build.sh
@@ -14,4 +14,7 @@ tar c --exclude=.svn --exclude=.empty -C board/sdc/wb45n/rootfs-additions/ . | t
 [ -f $TARGETDIR/etc/init.d/S??usbhost ] \
 && chmod a-x $TARGETDIR/etc/init.d/S??usbhost
 
+[ -f $TARGETDIR/etc/init.d/S??lighttpd ] \
+&& chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
+
 echo "WB45n POST BUILD script: done."
