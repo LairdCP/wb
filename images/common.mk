fd4e0ba1b02f3647a266a1da6310f3722831057a
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-10-21 16:43:53 -0700

    Truncated history, get it from B_prebuiltroot_history

---
+++ images/common.mk
@@ -0,0 +1,24 @@
+
+# PRODUCT must be set to wb40n or wb45n
+
+URL = http://$(shell hostname)/wb/$(PRODUCT)
+
+TOPDIR = ../../..
+IMAGES = $(TOPDIR)/buildroot/output/$(PRODUCT)/images
+
+fw.txt: copyall
+	$(TOPDIR)/buildroot/board/laird/mkfwtxt.sh $(URL)/$(DATE)
+
+copyall:
+	cp $(IMAGES)/kernel.bin .
+	cp $(IMAGES)/rootfs.bin .
+	cp $(IMAGES)/u-boot.bin .
+	cp $(IMAGES)/bootstrap.bin .
+	cp $(IMAGES)/fw_update .
+	cp $(IMAGES)/fw_select .
+	cp $(IMAGES)/rootfs.tar .
+	rm -f rootfs.tar.bz2
+	bzip2 rootfs.tar
+
+.PHONY: all copyall
+
