d5f3745b3277ed9db551a817e7628fc7791fe848
Chris Feyerchak <chris.feyerchak@lairdtech.com>
2017-04-28 16:13:52 -0400

    Make LWB5 FCC firmware tarball and zip file
    bug 11132
    
    Add makefile targets to create archives:
    
    * laird-lwb5-fcc-*.tar.bz2
    * 480-0081-*.zip
    
    These archives contain the LWB5's:
    
    * Wi-Fi firmware
    * Wi-Fi calibration file
    * Bluetooth firmware
    
    Features:
    
    1) multiple regulatory domains can co-exist and be selected by single symlink
    2) firmware can be completely installed by single "tar xf" command run from "/" working directory

--- sterling_packages.mk
+++ sterling_packages.mk
@@ -7,15 +7,17 @@ LAIRD_RELEASE_STRING ?= $(shell date +%Y%m%d)
 
 ST_FCC_NAME := laird-sterling-fcc-$(LAIRD_RELEASE_STRING)
 ST_ETSI_NAME := laird-sterling-etsi-$(LAIRD_RELEASE_STRING)
+LWB5_FCC_NAME := laird-lwb5-fcc-$(LAIRD_RELEASE_STRING)
 
 ST_FCC_OUT := $(ST_OUT)/$(ST_FCC_NAME)
 ST_ETSI_OUT := $(ST_OUT)/$(ST_ETSI_NAME)
+LWB5_FCC_OUT := $(ST_OUT)/$(LWB5_FCC_NAME)
 
 ST_BRCM_DIR := $(PWD)/buildroot/package/lrd-closed-source/externals/firmware/brcm
 
 ST_IMAGE_DIR := images/sterling
 
-all: sterling-fcc sterling-etsi
+all: sterling-fcc sterling-etsi lwb5-fcc
 
 #############################################################################
 # Support targets
@@ -35,6 +37,16 @@ images/sterling/$(ST_FCC_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR))
 # $(@F) is the file part of the target
 images/sterling/$(ST_ETSI_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
 	cd $(ST_OUT) ; tar -cjf $(@F) $(ST_ETSI_NAME)
+	cp $(ST_OUT)/$(@F) $@
+
+# $(@F) is the file part of the target
+images/sterling/$(LWB5_FCC_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
+	cd $(LWB5_FCC_OUT) ; tar -cjf $(ST_OUT)/$(@F) .
+	cp $(ST_OUT)/$(@F) $@
+
+# $(@F) is the file part of the target
+images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip: images/sterling/$(LWB5_FCC_NAME).tar.bz2
+	cd $(ST_OUT) ; zip $(@F) $(LWB5_FCC_NAME).tar.bz2
 	cp $(ST_OUT)/$(@F) $@
 
 sterling-fcc-staging: $(ST_OUT)
@@ -57,21 +69,39 @@ sterling-etsi-staging: $(ST_OUT)
 	ln -sf bcmdhd_4343w_etsi-*.cal brcmfmac43430-sdio.txt;\
 	ln -sf fw_bcmdhd_4343w-*.bin brcmfmac43430-sdio.bin
 
+lwb5-fcc-staging: $(ST_OUT)
+	mkdir -p $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339/region-fcc
+	cp -rad $(ST_BRCM_DIR)/bcm4339/* $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339/region-fcc
+	echo $(LAIRD_RELEASE_STRING) > $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339/region-fcc/laird-release
+	cd $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339;\
+        ln -sf ./region-fcc ./region ;\
+	ln -sf ./region/4339.hcd . ;\
+	ln -sf ./region/brcmfmac4339-sdio.txt . ;\
+	ln -sf ./region/brcmfmac4339-sdio.bin .
+	cd $(LWB5_FCC_OUT)/lib/firmware/brcm;\
+	ln -sf ./bcm4339/4339.hcd . ;\
+	ln -sf ./bcm4339/brcmfmac4339-sdio.txt . ;\
+	ln -sf ./bcm4339/brcmfmac4339-sdio.bin .
+
 #############################################################################
 #  clean targets
 clean-all:
 	rm -rf $(ST_OUT)
 	rm -f images/sterling/$(ST_FCC_NAME).tar.bz2
 	rm -f images/sterling/$(ST_ETSI_NAME).tar.bz2
+	rm -f images/sterling/$(LWB5_FCC_NAME).tar.bz2
+	rm -f images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
 
 clean:
 	rm -rf $(ST_FCC_OUT)
 	rm -rf $(ST_ETSI_OUT)
 	rm -f $(ST_OUT)/$(ST_FCC_NAME).tar.bz2
 	rm -r $(ST_OUT)/$(ST_ETSI_NAME).tar.bz2
+	rm -r $(ST_OUT)/480-0081-$(LAIRD_RELEASE_STRING).zip
 	rm -f images/sterling/$(ST_FCC_NAME).tar.bz2
 	rm -f images/sterling/$(ST_ETSI_NAME).tar.bz2
-
+	rm -f images/sterling/$(LWB5_FCC_NAME).tar.bz2
+	rm -f images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
 
 clean-nuke:
 	rm -rf $(ST_OUT)
@@ -84,4 +114,7 @@ sterling-fcc: sterling-fcc-staging images/sterling/$(ST_FCC_NAME).tar.bz2
 
 sterling-etsi: sterling-etsi-staging images/sterling/$(ST_ETSI_NAME).tar.bz2
 
-.PHONY: all sterling-fcc sterling-etsi clean clean-all clean-nuke
+lwb5-fcc: lwb5-fcc-staging images/sterling/$(LWB5_FCC_NAME).tar.bz2 images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
+
+
+.PHONY: all sterling-fcc sterling-etsi lwb5-fcc clean clean-all clean-nuke
