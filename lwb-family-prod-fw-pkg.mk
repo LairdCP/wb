b76afc377253acde19f9c3aa97128ba2f2fe67a2
Chris Feyerchak <chris.feyerchak@lairdtech.com>
2017-09-01 15:32:58 -0400

    sterling_packages.mk: Add LWB5 production firmware packages, factor-out LWB/LWB5 production firmware package makefile
    bug 11807

---
+++ lwb-family-prod-fw-pkg.mk
@@ -0,0 +1,84 @@
+#
+# This makefile uses a lot of automatic variables. For help, refer to
+# https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html
+#
+
+FW_PKG_LSR_PN ?= 000-0000
+BRCMFMAC_CHIP_ID ?= 00000
+CHIP_NAME ?= 0000
+REGION ?= xyz
+
+FW_REPO_DIR ?= ./buildroot/package/lrd-closed-source/externals/firmware
+OUT_FILE ?= $(PWD)/$(FW_PKG_LSR_PN).zip
+BUILD_DIR ?= $(dir $(OUT_FILE))/build-$(FW_PKG_LSR_PN)
+
+###############################################################################
+
+OUT_DIR := $(dir $(OUT_FILE))
+ARCHIVE_ROOT := $(BUILD_DIR)/archive
+HINT_FILE := $(OUT_FILE)-$(CHIP_NAME)-$(REGION).hint
+
+ZIP := zip
+TAR_F := tar --owner=root --group=root -cjf
+CP_F := cp -f
+
+$(OUT_FILE): $(BUILD_DIR)/$(FW_PKG_LSR_PN).zip | $(OUT_DIR) $(HINT_FILE)
+	cp -f $< $@
+
+$(HINT_FILE): $(OUT_DIR)
+	touch $@
+
+$(OUT_DIR):
+	mkdir -p $@
+
+$(BUILD_DIR)/$(FW_PKG_LSR_PN).zip: $(BUILD_DIR)/$(FW_PKG_LSR_PN).tgz
+	cd $(BUILD_DIR); $(ZIP) $@ $(FW_PKG_LSR_PN).tgz
+
+$(BUILD_DIR)/$(FW_PKG_LSR_PN).tgz: $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt $(ARCHIVE_ROOT)/lib/firmware/brcm/$(CHIP_NAME).hcd | $(OUT_DIR)
+	cd $(ARCHIVE_ROOT); $(TAR_F) $@ .
+
+#
+# $(ARCHIVE_ROOT)/lib/firmware/brcm
+#
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin
+	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin $(@F)
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt
+	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt $(@F)
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/$(CHIP_NAME).hcd: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/$(CHIP_NAME).hcd
+	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/$(CHIP_NAME).hcd $(@F)
+
+#
+# $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
+#
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin
+	cd $(@D); ln -sf ./brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin $(@F)
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt
+	cd $(@D); ln -sf ./brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt $(@F)
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
+	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin $@
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
+	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt $@
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/$(CHIP_NAME).hcd: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/$(CHIP_NAME).hcd | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
+	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/$(CHIP_NAME).hcd $@
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME):
+	mkdir -p $@
+
+#
+# Miscellaneous
+#
+
+clean:
+	rm -f $(OUT_FILE)
+	rm -f $(HINT_FILE)
+	rm -rf $(BUILD_DIR)
+
+.PHONY: clean
--- sterling_packages.mk
+++ sterling_packages.mk
@@ -5,20 +5,15 @@ ST_OUT := $(PWD)/buildroot/output/sterling
 
 LAIRD_RELEASE_STRING ?= $(shell date +%Y%m%d)
 
-LWB_FCC_NAME := laird-lwb-firmware-fcc-$(LAIRD_RELEASE_STRING)
-LWB_ETSI_NAME := laird-lwb-firmware-etsi-$(LAIRD_RELEASE_STRING)
 LWB_MFG_NAME := laird-lwb-firmware-mfg-$(LAIRD_RELEASE_STRING)
-
-LWB5_FCC_NAME := laird-lwb5-firmware-fcc-$(LAIRD_RELEASE_STRING)
 LWB5_MFG_NAME := laird-lwb5-firmware-mfg-$(LAIRD_RELEASE_STRING)
+
 60_NAME := laird-sterling-60-$(LAIRD_RELEASE_STRING)
 WL_FMAC_930_0081_NAME := 930-0081-$(LAIRD_RELEASE_STRING)
 
-LWB_FCC_OUT := $(ST_OUT)/$(LWB_FCC_NAME)
-LWB_ETSI_OUT := $(ST_OUT)/$(LWB_ETSI_NAME)
 LWB_MFG_OUT := $(ST_OUT)/$(LWB_MFG_NAME)
-LWB5_FCC_OUT := $(ST_OUT)/$(LWB5_FCC_NAME)
 LWB5_MFG_OUT := $(ST_OUT)/$(LWB5_MFG_NAME)
+
 60_OUT := $(ST_OUT)/$(60_NAME)
 
 ST_BRCM_DIR := $(PWD)/buildroot/package/lrd-closed-source/externals/firmware/brcm
@@ -28,7 +23,44 @@ ST_IMAGE_DIR := images/sterling
 
 TAR_CJF := tar --owner=root --group=root -cjf
 
-all: lwb-fcc lwb-etsi lwb-mfg lwb5-fcc lwb5-mfg 60 wl
+all: lwb-mfg lwb5-mfg 60 wl \
+	$(ST_IMAGE_DIR)/480-0079-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0080-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0081-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0082-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0094-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0095-$(LAIRD_RELEASE_STRING).zip
+
+
+#############################################################################
+# LWB/LWB5 production firmware packages
+
+MAKE_LWB_FAM_PROD_FW_PKG := $(MAKE) -f lwb-family-prod-fw-pkg.mk FW_REPO_DIR=$(PWD)/buildroot/package/lrd-closed-source/externals/firmware
+
+480_0079_PARAMS := FW_PKG_LSR_PN=480-0079 BRCMFMAC_CHIP_ID=43430 CHIP_NAME=4343w REGION=fcc  BUILD_DIR=$(ST_OUT)/480-0079 OUT_FILE=$(ST_IMAGE_DIR)/480-0079-$(LAIRD_RELEASE_STRING).zip
+480_0080_PARAMS := FW_PKG_LSR_PN=480-0080 BRCMFMAC_CHIP_ID=43430 CHIP_NAME=4343w REGION=etsi BUILD_DIR=$(ST_OUT)/480-0080 OUT_FILE=$(ST_IMAGE_DIR)/480-0080-$(LAIRD_RELEASE_STRING).zip
+480_0081_PARAMS := FW_PKG_LSR_PN=480-0081 BRCMFMAC_CHIP_ID=4339  CHIP_NAME=4339  REGION=fcc  BUILD_DIR=$(ST_OUT)/480-0081 OUT_FILE=$(ST_IMAGE_DIR)/480-0081-$(LAIRD_RELEASE_STRING).zip
+480_0082_PARAMS := FW_PKG_LSR_PN=480-0082 BRCMFMAC_CHIP_ID=4339  CHIP_NAME=4339  REGION=etsi BUILD_DIR=$(ST_OUT)/480-0082 OUT_FILE=$(ST_IMAGE_DIR)/480-0082-$(LAIRD_RELEASE_STRING).zip
+480_0094_PARAMS := FW_PKG_LSR_PN=480-0094 BRCMFMAC_CHIP_ID=4339  CHIP_NAME=4339  REGION=ic   BUILD_DIR=$(ST_OUT)/480-0094 OUT_FILE=$(ST_IMAGE_DIR)/480-0094-$(LAIRD_RELEASE_STRING).zip
+480_0095_PARAMS := FW_PKG_LSR_PN=480-0095 BRCMFMAC_CHIP_ID=4339  CHIP_NAME=4339  REGION=jp   BUILD_DIR=$(ST_OUT)/480-0095 OUT_FILE=$(ST_IMAGE_DIR)/480-0095-$(LAIRD_RELEASE_STRING).zip
+
+$(ST_IMAGE_DIR)/480-0079-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0079_PARAMS)
+
+$(ST_IMAGE_DIR)/480-0080-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0080_PARAMS)
+
+$(ST_IMAGE_DIR)/480-0081-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0081_PARAMS)
+
+$(ST_IMAGE_DIR)/480-0082-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0082_PARAMS)
+
+$(ST_IMAGE_DIR)/480-0094-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0094_PARAMS)
+
+$(ST_IMAGE_DIR)/480-0095-$(LAIRD_RELEASE_STRING).zip:
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0095_PARAMS)
 
 #############################################################################
 # Support targets
@@ -41,16 +73,6 @@ $(ST_IMAGE_DIR):
 	mkdir -p $(ST_IMAGE_DIR)
 
 # $(@F) is the file part of the target
-images/sterling/$(LWB_FCC_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
-	cd $(ST_OUT)/$(LWB_FCC_NAME) ; $(TAR_CJF) ../$(@F) .
-	cp $(ST_OUT)/$(@F) $@
-
-# $(@F) is the file part of the target
-images/sterling/$(LWB_ETSI_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
-	cd $(ST_OUT)/$(LWB_ETSI_NAME) ; $(TAR_CJF) ../$(@F) .
-	cp $(ST_OUT)/$(@F) $@
-
-# $(@F) is the file part of the target
 images/sterling/$(LWB_MFG_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
 	cd $(ST_OUT)/$(LWB_MFG_NAME) ; $(TAR_CJF) ../$(@F) .
 	cp $(ST_OUT)/$(@F) $@
@@ -58,11 +80,6 @@ images/sterling/$(LWB_MFG_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)
 # $(@F) is the file part of the target
 images/sterling/480-0108-$(LAIRD_RELEASE_STRING).zip: images/sterling/$(LWB_MFG_NAME).tar.bz2
 	cd $(ST_OUT) ; zip $(@F) $(LWB_MFG_NAME).tar.bz2
-	cp $(ST_OUT)/$(@F) $@
-
-# $(@F) is the file part of the target
-images/sterling/$(LWB5_FCC_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
-	cd $(LWB5_FCC_OUT) ; $(TAR_CJF) $(ST_OUT)/$(@F) lib
 	cp $(ST_OUT)/$(@F) $@
 
 # $(@F) is the file part of the target
@@ -76,43 +93,12 @@ images/sterling/480-0109-$(LAIRD_RELEASE_STRING).zip: images/sterling/$(LWB5_MFG
 	cp $(ST_OUT)/$(@F) $@
 
 # $(@F) is the file part of the target
-images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip: images/sterling/$(LWB5_FCC_NAME).tar.bz2
-	cd $(ST_OUT) ; zip $(@F) $(LWB5_FCC_NAME).tar.bz2
-	cp $(ST_OUT)/$(@F) $@
-
-# $(@F) is the file part of the target
 images/sterling/$(60_NAME).tar.bz2: $(filter-out $(wildcard $(ST_IMAGE_DIR)), $(ST_IMAGE_DIR))
 	cd $(60_OUT) ; $(TAR_CJF) $(ST_OUT)/$(@F) lib
 	cp $(ST_OUT)/$(@F) $@
 
 images/sterling/$(WL_FMAC_930_0081_NAME).zip: $(ST_OUT)/$(WL_FMAC_930_0081_NAME).zip | $(ST_IMAGE_DIR)
 	cp -f $^ $@
-
-lwb-fcc-staging: $(ST_OUT)
-	mkdir -p $(LWB_FCC_OUT)/lib/firmware/brcm/bcm4343w
-	cd $(LWB_FCC_OUT)/lib/firmware/brcm/bcm4343w ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/brcmfmac43430-sdio-prod.bin . ; \
-	ln -s brcmfmac43430-sdio-prod.bin brcmfmac43430-sdio.bin ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/brcmfmac43430-sdio-fcc.txt . ; \
-	ln -s brcmfmac43430-sdio-fcc.txt brcmfmac43430-sdio.txt ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/4343w.hcd . ; \
-	cd $(LWB_FCC_OUT)/lib/firmware/brcm ; \
-	ln -sf ./bcm4343w/brcmfmac43430-sdio.bin . ; \
-	ln -sf ./bcm4343w/brcmfmac43430-sdio.txt . ; \
-	ln -sf ./bcm4343w/4343w.hcd .
-
-lwb-etsi-staging: $(ST_OUT)
-	mkdir -p $(LWB_ETSI_OUT)/lib/firmware/brcm/bcm4343w
-	cd $(LWB_ETSI_OUT)/lib/firmware/brcm/bcm4343w ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/brcmfmac43430-sdio-prod.bin . ; \
-	ln -s brcmfmac43430-sdio-prod.bin brcmfmac43430-sdio.bin ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/brcmfmac43430-sdio-etsi.txt . ; \
-	ln -s brcmfmac43430-sdio-etsi.txt brcmfmac43430-sdio.txt ; \
-	cp $(ST_BRCM_DIR)/bcm4343w/4343w.hcd .
-	cd $(LWB_ETSI_OUT)/lib/firmware/brcm ; \
-	ln -sf ./bcm4343w/brcmfmac43430-sdio.bin . ; \
-	ln -sf ./bcm4343w/brcmfmac43430-sdio.txt . ; \
-	ln -sf ./bcm4343w/4343w.hcd .
 
 lwb-mfg-staging: $(ST_OUT)
 	mkdir -p $(LWB_MFG_OUT)/lib/firmware/brcm/bcm4343w
@@ -126,19 +112,6 @@ lwb-mfg-staging: $(ST_OUT)
 	ln -sf ./bcm4343w/brcmfmac43430-sdio.bin . ; \
 	ln -sf ./bcm4343w/brcmfmac43430-sdio.txt . ; \
 	ln -sf ./bcm4343w/4343w.hcd .
-
-lwb5-fcc-staging: $(ST_OUT)
-	mkdir -p $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339
-	cd $(LWB5_FCC_OUT)/lib/firmware/brcm/bcm4339 ; \
-	cp $(ST_BRCM_DIR)/bcm4339/brcmfmac4339-sdio-prod.bin . ; \
-	ln -s brcmfmac4339-sdio-prod.bin brcmfmac4339-sdio.bin ; \
-	cp $(ST_BRCM_DIR)/bcm4339/brcmfmac4339-sdio-fcc.txt . ; \
-	ln -s brcmfmac4339-sdio-fcc.txt brcmfmac4339-sdio.txt ; \
-	cp $(ST_BRCM_DIR)/bcm4339/4339.hcd . ; \
-	cd $(LWB5_FCC_OUT)/lib/firmware/brcm ; \
-	ln -sf ./bcm4339/brcmfmac4339-sdio.bin . ; \
-	ln -sf ./bcm4339/brcmfmac4339-sdio.txt . ; \
-	ln -sf ./bcm4339/4339.hcd .
 
 lwb5-mfg-staging: $(ST_OUT)
 	mkdir -p $(LWB5_MFG_OUT)/lib/firmware/brcm/bcm4339
@@ -165,39 +138,32 @@ $(ST_OUT)/$(WL_FMAC_930_0081_NAME).zip: buildroot/package/lrd-closed-source/exte
 #  clean targets
 clean-all:
 	rm -rf $(ST_OUT)
-	rm -f images/sterling/$(LWB_FCC_NAME).tar.bz2
-	rm -f images/sterling/$(LWB_ETSI_NAME).tar.bz2
-	rm -f images/sterling/$(LWB5_FCC_NAME).tar.bz2
 	rm -f images/sterling/$(LWB5_MFG_NAME).tar.bz2
-	rm -f images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
 	rm -f images/sterling/480-0108-$(LAIRD_RELEASE_STRING).zip
 	rm -f images/sterling/480-0109-$(LAIRD_RELEASE_STRING).zip
 	rm -f images/sterling/$(60_NAME).tar.bz2
 
 clean:
-	rm -rf $(LWB_FCC_OUT)
-	rm -rf $(LWB_ETSI_OUT)
 	rm -rf $(LWB_MFG_OUT)
-	rm -rf $(LWB5_FCC_OUT)
 	rm -rf $(LWB5_MFG_OUT)
 	rm -rf $(60_OUT)
-	rm -f $(ST_OUT)/$(LWB_FCC_NAME).tar.bz2
-	rm -f $(ST_OUT)/$(LWB_ETSI_NAME).tar.bz2
 	rm -f $(ST_OUT)/$(LWB_MFG_NAME).tar.bz2
-	rm -f $(ST_OUT)/$(LWB5_FCC_NAME).tar.bz2
 	rm -f $(ST_OUT)/$(LWB5_MFG_NAME).tar.bz2
-	rm -f $(ST_OUT)/480-0081-$(LAIRD_RELEASE_STRING).zip
 	rm -f $(ST_OUT)/480-0108-$(LAIRD_RELEASE_STRING).zip
 	rm -f $(ST_OUT)/480-0109-$(LAIRD_RELEASE_STRING).zip
 	rm -f $(ST_OUT)/$(WL_FMAC_930_0081_NAME).zip
-	rm -f images/sterling/$(LWB_FCC_NAME).tar.bz2
-	rm -f images/sterling/$(LWB_ETSI_NAME).tar.bz2
 	rm -f images/sterling/$(LWB_MFG_NAME).tar.bz2
-	rm -f images/sterling/$(LWB5_FCC_NAME).tar.bz2
 	rm -f images/sterling/$(LWB5_MFG_NAME).tar.bz2
-	rm -f images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
 	rm -f images/sterling/$(WL_FMAC_930_0081_NAME).zip
 	rm -f images/sterling/$(60_NAME).tar.bz2
+	rm -f images/sterling/480-0108-$(LAIRD_RELEASE_STRING).zip
+	rm -f images/sterling/480-0109-$(LAIRD_RELEASE_STRING).zip
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0079_PARAMS) clean
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0080_PARAMS) clean
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0081_PARAMS) clean
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0082_PARAMS) clean
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0094_PARAMS) clean
+	$(MAKE_LWB_FAM_PROD_FW_PKG) $(480_0095_PARAMS) clean
 
 clean-nuke:
 	rm -rf $(ST_OUT)
@@ -206,13 +172,7 @@ clean-nuke:
 #############################################################################
 # top level targets
 
-lwb-fcc: lwb-fcc-staging images/sterling/$(LWB_FCC_NAME).tar.bz2
-
-lwb-etsi: lwb-etsi-staging images/sterling/$(LWB_ETSI_NAME).tar.bz2
-
 lwb-mfg: lwb-mfg-staging images/sterling/$(LWB_MFG_NAME).tar.bz2 images/sterling/480-0108-$(LAIRD_RELEASE_STRING).zip
-
-lwb5-fcc: lwb5-fcc-staging images/sterling/$(LWB5_FCC_NAME).tar.bz2 images/sterling/480-0081-$(LAIRD_RELEASE_STRING).zip
 
 lwb5-mfg: lwb5-mfg-staging images/sterling/$(LWB5_MFG_NAME).tar.bz2 images/sterling/480-0109-$(LAIRD_RELEASE_STRING).zip
 
@@ -220,4 +180,10 @@ lwb5-mfg: lwb5-mfg-staging images/sterling/$(LWB5_MFG_NAME).tar.bz2 images/sterl
 
 wl: images/sterling/$(WL_FMAC_930_0081_NAME).zip
 
-.PHONY: all lwb-fcc lwb-etsi lwb-mfg lwb5-fcc lwb5-mfg 60 wl clean clean-all clean-nuke
+.PHONY: all lwb-mfg lwb5-mfg 60 wl clean clean-all clean-nuke \
+	$(ST_IMAGE_DIR)/480-0079-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0080-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0081-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0082-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0094-$(LAIRD_RELEASE_STRING).zip \
+	$(ST_IMAGE_DIR)/480-0095-$(LAIRD_RELEASE_STRING).zip
