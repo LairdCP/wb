9379d47e506b4c0ccd3f959fd50bdb59d40a3942
Eric Bentley <eric.bentley@lairdtech.com>
2018-04-30 23:58:15 -0400

    sterling-lwb-firmware: selectively add .clm_blob file
    Currently the 4343w firmware package contains a .clm_blob file while the
    4339 does not, but is expected to have one in the future.  Here the
    determination of including the .clm_blob file is done by checking if one
    exists in the main repo, and if so, adding it as a dependency.
    The variable will be empty if the file does not exist so no dependency
    will exist.

--- lwb-family-prod-fw-pkg.mk
+++ lwb-family-prod-fw-pkg.mk
@@ -22,6 +22,12 @@ ZIP := zip
 TAR_F := tar --owner=root --group=root -cjf
 CP_F := cp -f
 
+#  .clm_blob files may or may not be present for a particular
+#  $(BRCMFMAC_CHIP_ID).  If present, we need to include it.  If not
+#  present, this will expand to an empty string so there will not be a 
+#  dependency for it.
+BLOB_FILE := $(shell [ -e $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob ] && echo $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob)
+
 $(OUT_FILE): $(BUILD_DIR)/$(FW_PKG_LSR_PN).zip | $(OUT_DIR) $(HINT_FILE)
 	cp -f $< $@
 
@@ -34,7 +40,7 @@ $(OUT_DIR):
 $(BUILD_DIR)/$(FW_PKG_LSR_PN).zip: $(BUILD_DIR)/$(FW_PKG_LSR_PN).tar.bz2
 	cd $(BUILD_DIR); $(ZIP) $@ $(FW_PKG_LSR_PN).tar.bz2
 
-$(BUILD_DIR)/$(FW_PKG_LSR_PN).tar.bz2: $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt $(ARCHIVE_ROOT)/lib/firmware/brcm/$(CHIP_NAME).hcd | $(OUT_DIR)
+$(BUILD_DIR)/$(FW_PKG_LSR_PN).tar.bz2: $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt $(ARCHIVE_ROOT)/lib/firmware/brcm/$(CHIP_NAME).hcd $(BLOB_FILE) | $(OUT_DIR)
 	cd $(ARCHIVE_ROOT); $(TAR_F) $@ lib
 
 #
@@ -43,6 +49,9 @@ $(BUILD_DIR)/$(FW_PKG_LSR_PN).tar.bz2: $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfma
 
 $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin
 	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.bin $(@F)
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob
+	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob $(@F)
 
 $(ARCHIVE_ROOT)/lib/firmware/brcm/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt: $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt
 	cd $(@D); ln -sf ./bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.txt $(@F)
@@ -62,6 +71,9 @@ $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sd
 
 $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
 	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-prod.bin $@
+
+$(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
+	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio.clm_blob $@
 
 $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt: $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt | $(ARCHIVE_ROOT)/lib/firmware/brcm/bcm$(CHIP_NAME)
 	$(CP_F) $(FW_REPO_DIR)/brcm/bcm$(CHIP_NAME)/brcmfmac$(BRCMFMAC_CHIP_ID)-sdio-$(REGION).txt $@
