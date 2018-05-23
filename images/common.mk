3d5cbfb2d43ad32bef7a9af389fc05f8116d79cf
Ben Whitten <ben.whitten@lairdtech.com>
2018-05-23 15:18:56 +0100

    laird: copy files of interest from multiple targets
    The WB targets need a differing set of files then the SOM, instead of
    testing file availability and conditionally adding to transfer list.
    Make a global list of all files of interest and test availability before
    copying.
    
    Bug 12979

--- images/common.mk
+++ images/common.mk
@@ -6,55 +6,29 @@ URL = http://$(shell hostname)/wb/$(PRODUCT)
 TOPDIR ?= ../../..
 IMAGES = $(TOPDIR)/buildroot/output/$(PRODUCT)/images
 
-USERBIN_EXISTS := $(shell [ -e $(IMAGES)/userfs.bin ] && echo 1 || echo 0)
-SQROOT_EXISTS := $(shell [ -e $(IMAGES)/sqroot.bin ] && echo 1 || echo 0)
-BOOTSTRAP_EXISTS := $(shell [ -e $(IMAGES)/at91bs.bin ] && echo 1 || echo 0)
-UBOOT_EXISTS := $(shell [ -e $(IMAGES)/u-boot.bin ] && echo 1 || echo 0)
-SWU_EXISTS := $(shell [ -e $(IMAGES)/*_$(DATE).swu ] && echo 1 || echo 0)
-
-FW_UPDATE_EXISTS := $(shell [ -e $(IMAGES)/fw_update ] && echo 1 || echo 0)
-FW_SELECT_EXISTS := $(shell [ -e $(IMAGES)/fw_select ] && echo 1 || echo 0)
-FW_USI_EXISTS := $(shell [ -e $(IMAGES)/fw_usi ] && echo 1 || echo 0)
-FW_TXT_EXISTS := $(shell [ -e $(IMAGES)/fw.txt ] && echo 1 || echo 0)
-
 # General files
-FILES := kernel.bin rootfs.bin rootfs.tar
+FILES := kernel.bin kernel.itb rootfs.bin rootfs.tar rootfs.tar.bz2
 
-ifeq ($(BOOTSTRAP_EXISTS),1)
+# At91bootstrap
 FILES += at91bs.bin
-endif
 
-ifeq ($(UBOOT_EXISTS),1)
-FILES += u-boot.bin
-endif
+# U-Boot SPL
+FILES += boot.bin
 
-ifeq ($(USERBIN_EXISTS),1)
+# U-Boot propper
+FILES += u-boot.bin u-boot.itb
+
+# RW User filesystem
 FILES += userfs.bin
-endif
 
-ifeq ($(SQROOT_EXISTS),1)
+# RO Squashfs root
 FILES += sqroot.bin
-endif
 
-ifeq ($(SWU_EXISTS),1)
-FILES += *_$(DATE).swu
-endif
+# SW Update file
+FILES += $(PRODUCT)_$(DATE).swu
 
-ifeq ($(FW_UPDATE_EXISTS),1)
-FILES += fw_update
-endif
-
-ifeq ($(FW_SELECT_EXISTS),1)
-FILES += fw_select
-endif
-
-ifeq ($(FW_USI_EXISTS),1)
-FILES += fw_usi
-endif
-
-ifeq ($(FW_TXT_EXISTS),1)
-FILES += fw.txt
-endif
+# FW_update files
+FILES += fw_update fw_select fw_usi fw.txt
 
 legal-info:
 	rsync -a --exclude=sources $(TOPDIR)/buildroot/output/$(PRODUCT)/legal-info/ ./legal-info-$(DATE)
@@ -64,9 +38,8 @@ legal-info:
 	rm -rf ./legal-info-$(DATE)
 
 copyall:
-	$(foreach FILE,$(FILES), cp $(IMAGES)/$(FILE) .;)
-	rm -f rootfs.tar.bz2
-	bzip2 rootfs.tar
+	$(foreach FILE,$(FILES), $(shell [ -e $(IMAGES)/$(FILE) ] && cp $(IMAGES)/$(FILE) .))
+	$(shell [ -e rootfs.tar.bz2 ] && rm -f rootfs.tar.bz2 && bzip2 rootfs.tar;)
 
 bdimx6:
 	cp $(IMAGES)/sdcard.img . -fr
